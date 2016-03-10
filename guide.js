dd53a06db3286fe6c75828b74fe19bfc1aa3f7db
redsamba <willtimo@oregonstate.edu>
2016-03-09 22:52:07 -0800

    Create guide.js

---
+++ guide.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+
+  res.render('guideMain.handlebars');
+
+});
